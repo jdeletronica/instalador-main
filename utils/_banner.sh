84368404f4aefce4397e84aedc50e3ae08c29989
jdeletronica <124458420+jdeletronica@users.noreply.github.com>
2024-06-04 09:07:15 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -24,10 +24,14 @@ print_banner() {
 printf "${CYAN_LIGHT}";
 
 
-printf ${CYAN_LIGHT}" DIGITEC-INFORMATICA  \n";  
-
+printf ${CYAN_LIGHT}" ____   ____  ________  ____    ____   ________    _       ________  ________  _______   \n";  
+printf ${CYAN_LIGHT}"|_  _| |_  _||_   __  ||_   \  /   _| |_   __  |  / \     |  __   _||_   __  ||_   __ \  \n"; 
+printf ${CYAN_LIGHT}"  \ \   / /    | |_ \_|  |   \/   |     | |_ \_| / _ \    |_/  / /    | |_ \_|  | |__) | \n";
+printf ${CYAN_LIGHT}"   \ \ / /     |  _| _   | |\  /| |     |  _|   / ___ \      .'.' _   |  _| _   |  __ /  \n";
+printf ${CYAN_LIGHT}"    \ ' /     _| |__/ | _| |_\/_| |_   _| |_  _/ /   \ \_  _/ /__/ | _| |__/ | _| |  \ \_\n";
+printf ${CYAN_LIGHT}"     \_/     |________||_____||_____| |_____||____| |____||________||________||____| |___|\n";
                                                                                                                                                          
-  printf "            \033[1;33m 
+  printf "            \033[1;33m        ";
   printf "${NC}";
 
   printf "\n"
